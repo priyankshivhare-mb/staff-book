91ed3861c58e2573b685ed6a3c738b4569074920
priyankshivhare-mb <priyank.shivhare@mindbodyonline.com>
2021-06-26 22:42:48 +0530

    Minor refactor - added banner

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,23 +1,10 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
-import Home from '../home'
+import Dashboard from '../dashboard'
 import About from '../about'
 
 const App = () => (
     <>
-        <nav className="navbar navbar-inverse navbar-fixed-top">
-            <div className="container">
-                <div className="navbar-header">
-                    <a className="navbar-brand" href="#">Project name</a>
-                </div>
-                <div id="navbar" className="collapse navbar-collapse">
-                    <ul className="nav navbar-nav">
-                        <Link to="/">Home</Link>
-                        <Link to="/about-us">About</Link>
-                    </ul>
-                </div>
-            </div>
-        </nav>
         <div className="container">
             <header>
                 <Link to="/">Home</Link>
@@ -25,7 +12,7 @@ const App = () => (
             </header>
 
             <main>
-                <Route exact path="/" component={Home} />
+                <Route exact path="/" component={Dashboard} />
                 <Route exact path="/about-us" component={About} />
             </main>
         </div>
