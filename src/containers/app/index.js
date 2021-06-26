3e4a574f927983569b6a0a7b09779b9bfd8cb7ab
priyankshivhare-mb <priyank.shivhare@mindbodyonline.com>
2021-06-26 23:02:40 +0530

    Refined navbar, style fixes

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,19 +1,26 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
 import Dashboard from '../dashboard'
 import About from '../about'
 
 const App = () => (
     <>
+        <nav className="navbar navbar-expand-lg navbar-light nav-mb-theme">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/">Dashboard</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/about-us">About</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         <div className="container">
-            <header>
-                <Link to="/">Home</Link>
-                <Link to="/about-us">About</Link>
-            </header>
-
             <main>
-                <Route exact path="/" component={Dashboard} />
-                <Route exact path="/about-us" component={About} />
+                <Route exact path="/" component={Dashboard}/>
+                <Route exact path="/about-us" component={About}/>
             </main>
         </div>
     </>
