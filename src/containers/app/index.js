22944bcf5ea77546f34659d92f34d7c114b545b5
priyankshivhare-mb <priyank.shivhare@mindbodyonline.com>
2021-06-27 13:26:14 +0530

    Styling enhancements

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Route, Link} from 'react-router-dom'
 import Dashboard from '../dashboard'
 import About from '../about'
 
-const App = () => (
+const App = () => {
+    const [ activeLink, setActiveLink ] = useState('dashboard');
+
+    return (
     <>
         <nav className="navbar navbar-expand-lg navbar-light nav-mb-theme">
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/">Dashboard</a>
+                    <li className={`${activeLink === 'dashboard' ? 'active' : ''} nav-item`}>
+                        <Link className="nav-link" to="/" onClick={() => setActiveLink('dashboard')}>Dashboard</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/about-us">About</a>
+                        <Link className="nav-link" to="#">Setup Checklist</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">Staff</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">Services & Pricing</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">Products</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="#">Manager Tool</Link>
+                    </li>
+                    <li className={`${activeLink === 'aboutUs' ? 'active' : ''} nav-item`}>
+                        <Link className="nav-link" to="/about-us" onClick={() => setActiveLink('aboutUs')}>About us</Link>
                     </li>
                 </ul>
             </div>
@@ -24,6 +42,6 @@ const App = () => (
             </main>
         </div>
     </>
-)
+)}
 
 export default App
