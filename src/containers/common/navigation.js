0dbf1f91f033f5dd2114b21b0d3dadeb9e136082
Moiz Mansur <moiz.mansur@mindbodyonline.com>
2021-07-06 10:05:04 +0530

    chore: update the navbar with icons, WIP since icons arent perfect

--- src/containers/common/navigation.js
+++ src/containers/common/navigation.js
@@ -1,36 +1,53 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import StaffBookLogo from '../../assets/staff-book-logo.png';
 
 const Navigation = (props) => {
-    return (
-        <div className="container">
-            <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <ul className="nav nav-pills">
-                    <li className={`${props.activeLink === 'dashboard' ? 'active' : ''} nav-item`}>
-                        <Link className="nav-link" to="/" onClick={() => props.setActiveLink('dashboard')}>Dashboard</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="#">Setup Checklist</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="#">Staff</Link>
-                    </li>
-                    { props.isStaffBookEnabled && <li className={`${props.activeLink === 'staffBook' ? 'active' : ''} nav-item`}>
-                        <Link className="nav-link" to="/staff-book" onClick={() => props.setActiveLink('staffBook')}>Staff Book</Link>
-                    </li> }
-                    <li className="nav-item">
-                        <Link className="nav-link" to="#">Services & Pricing</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="#">Products</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="#">Manager Tools</Link>
-                    </li>
-                </ul>
-            </header>
-        </div>
-    );
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <a className="navbar-brand" href="/">
+        <img src={StaffBookLogo} height="80" alt="" />
+      </a>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#"><i className="fa fa-th-large" /> Dashboard</a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              <i className="fa fa-file-text-o" /> Search Profile
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              <i className="fa fa-file-text-o" /> Shortlisted Profiles
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              <i className="fa fa-file-text-o" /> Recommended Profiles
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              <i className="fa fa-file-text-o" /> Candidates Status
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              <i className="fa fa-bell-o" />
+            </a>
+          </li>
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              <i className="fa fa-cog" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
