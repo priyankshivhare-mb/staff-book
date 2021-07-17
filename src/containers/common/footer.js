a594b39675883f2142699489d01ceadaf327f6a8
Moiz Mansur <moiz.mansur@mindbodyonline.com>
2021-07-17 22:58:16 +0530

    convert footer to links

--- src/containers/common/footer.js
+++ src/containers/common/footer.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <div className="row pb-4">
       <div className="col-12 px-5">
-        <span className="footer-link pr-3">Terms and Conditions</span>
-        <span className="footer-link px-3">Privacy Policy</span>
-        <span className="footer-link px-3">Help</span>
-        <span className="footer-link px-3 no-border">Contact</span>
-        <span className="footer-link pl-3 no-border float-right">© All Rights Reserved.</span>
+        <Link className="footer-link pr-3">Terms and Conditions</Link>
+        <Link className="footer-link px-3">Privacy Policy</Link>
+        <Link className="footer-link px-3">Help</Link>
+        <Link className="footer-link px-3 no-border">Contact</Link>
+        <Link className="footer-link pl-3 no-border float-right">© All Rights Reserved.</Link>
       </div>
     </div>
   );
