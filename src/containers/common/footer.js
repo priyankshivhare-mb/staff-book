9ce9c1c7df131916d8da8cf6cf383cf88ae4783d
Moiz Mansur <moiz.mansur@mindbodyonline.com>
2021-07-18 12:10:37 +0530

    wrap footer in container

--- src/containers/common/footer.js
+++ src/containers/common/footer.js
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className="row pb-4">
-      <div className="col-12 px-5">
-        <Link className="footer-link pr-3">Terms and Conditions</Link>
-        <Link className="footer-link px-3">Privacy Policy</Link>
-        <Link className="footer-link px-3">Help</Link>
-        <Link className="footer-link px-3 no-border">Contact</Link>
-        <Link className="footer-link pl-3 no-border float-right">© All Rights Reserved.</Link>
+    <div className="container-fluid">
+      <div className="row pb-4">
+        <div className="col-12 px-5">
+          <Link className="footer-link pr-3">Terms and Conditions</Link>
+          <Link className="footer-link px-3">Privacy Policy</Link>
+          <Link className="footer-link px-3">Help</Link>
+          <Link className="footer-link px-3 no-border">Contact</Link>
+          <Link className="footer-link pl-3 no-border float-right">© All Rights Reserved.</Link>
+        </div>
       </div>
     </div>
   );
