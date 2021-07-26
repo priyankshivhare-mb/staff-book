21f035efe6b8a82f7e2d33abe49f8139a0c600d0
Moiz Mansur <moiz.mansur@mindbodyonline.com>
2021-07-26 19:32:11 +0530

    chore: change values in employee benefit card

--- src/containers/common/employeeBenefitCard.js
+++ src/containers/common/employeeBenefitCard.js
@@ -8,32 +8,32 @@ const EmployeeBenefitCard = (props) => {
                 <div className="row">
                     <div className="col-md-12 benefit-container">
                         <div className="benefit-row">
-                            <span className="highlight">99%</span> Work/Life Balance
-                            <span className="score pull-right">200</span>
+                            <span className="highlight">82%</span> Work/Life Balance
+                            <span className="score pull-right">32</span>
                         </div>
                         <div className="benefit-row">
-                            <span className="highlight">99%</span> Career Development
-                            <span className="score pull-right">200</span>
+                            <span className="highlight">90%</span> Career Development
+                            <span className="score pull-right">28</span>
                         </div>
                         <div className="benefit-row">
-                            <span className="highlight">99%</span> Compensation, Benefits & Perks
-                            <span className="score pull-right">200</span>
+                            <span className="highlight">89%</span> Compensation, Benefits & Perks
+                            <span className="score pull-right">39</span>
                         </div>
                         <div className="benefit-row">
-                            <span className="highlight">99%</span> Working Environment
-                            <span className="score pull-right">200</span>
+                            <span className="highlight">78%</span> Working Environment
+                            <span className="score pull-right">30</span>
                         </div>
                         <div className="benefit-row">
-                            <span className="highlight">99%</span> Diversity and Equal Opportunity
-                            <span className="score pull-right">200</span>
+                            <span className="highlight">93%</span> Diversity and Equal Opportunity
+                            <span className="score pull-right">27</span>
                         </div>
                         <div className="benefit-row">
-                            <span className="highlight">99%</span> Culture Rating
-                            <span className="score pull-right">200</span>
+                            <span className="highlight">89%</span> Culture Rating
+                            <span className="score pull-right">32</span>
                         </div>
                         <div className="benefit-row">
                             <span className="highlight">99%</span> Job Security
-                            <span className="score pull-right">200</span>
+                            <span className="score pull-right">30</span>
                         </div>
                     </div>
                 </div>
